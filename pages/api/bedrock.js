67df09350eb4f2101e40098b184356fda1bffe76
walsaid <wajih.alsaid@yahoo.com>
2025-03-30 17:14:03 +0400

    Bug fixes

--- pages/api/bedrock.js
+++ pages/api/bedrock.js
@@ -1,6 +1,6 @@
-import { SignatureV4 } from "@aws-sdk/signature-v4";
 import { Sha256 } from "@aws-crypto/sha256-js";
-import { HttpRequest } from "@aws-sdk/protocol-http";
+import { HttpRequest } from "@smithy/protocol-http";
+import { SignatureV4 } from "@smithy/signature-v4";
 
 
 const maskAccessKey = (AccessKey) => {
