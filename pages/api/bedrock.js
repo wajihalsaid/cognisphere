7e5d1bb2d639d5a831706f350888246504ec1bc6
walsaid <wajih.alsaid@yahoo.com>
2025-10-11 04:28:32 +0400

    Enhancements and adding Custom API Server

--- pages/api/bedrock.js
+++ pages/api/bedrock.js
@@ -1,7 +1,14 @@
+// polyfill.js - MUST BE FIRST
+import { File } from 'formdata-node';
+globalThis.File = File;
+
 import { Sha256 } from "@aws-crypto/sha256-js";
 import { HttpRequest } from "@smithy/protocol-http";
 import { SignatureV4 } from "@smithy/signature-v4";
 import { v4 as uuidv4 } from "uuid"; // Import UUID for session ID generation
+
+const { Agent } = await import('undici');
+
 
 const conversationMemoryBedrock = {}; // In-memory storage (resets on server restart)
 
@@ -72,7 +79,6 @@ export default async function handler(req, res) {
         ? CustomURL
         : `bedrock-runtime.${region}.amazonaws.com`;
     const path = `/model/${modelId}/converse`;
-  
 
     // Retrieve chat history from memory storage (using sessionId)
     if (!conversationMemoryBedrock[sessionId]) {
@@ -121,15 +127,30 @@ export default async function handler(req, res) {
       path: path,
       headers: {
         "Content-Type": "application/json",
-        "Accept-Encoding": "", // Temporrary solution for Server Gateway option
+        //"Accept-Encoding": "", // Temporrary solution for Server Gateway option
         accept: "application/json",
         Host: hostname,
       },
       body: JSON.stringify(requestPayload),
     });
     const signedRequest = await signer.sign(request);
-
+    let agent = undefined;
     if (aiDefenseMode === "gateway" && gatewayUrl) {
+      const allowedPrefixes = [
+        "https://us.gateway.aidefense",
+        "https://eu.gateway.aidefense",
+        "https://ap.gateway.aidefense",
+      ];
+      const shouldIgnoreCert = !allowedPrefixes.some((prefix) =>
+        gatewayUrl.startsWith(prefix)
+      );
+      agent = shouldIgnoreCert
+        ? new Agent({
+            connect: {
+              rejectUnauthorized: false,
+            },
+          })
+        : undefined;
       apiUrl = gatewayUrl + path; // Set the custom gateway URL
     }
 
@@ -147,12 +168,14 @@ export default async function handler(req, res) {
       url: apiUrl,
       headers: maskedSignedHeaders,
       body: signedRequest.body,
+      dispatcher: agent,
     };
 
     const response = await fetch(apiUrl, {
       method: signedRequest.method,
       headers: signedRequest.headers,
       body: signedRequest.body,
+      dispatcher: agent,
     });
 
     const headersObject = {};
